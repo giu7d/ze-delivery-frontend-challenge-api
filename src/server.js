bb06eefc141e284eaee54daa7cef69ae2d1a307f
Giuseppe Setem <giuseppe7d@gmail.com>
2022-04-20 13:12:11 -0300

    feat: create json server

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const jsonServer = require("json-server");
+
+const server = jsonServer.create();
+
+const router = jsonServer.router("data/db.json");
+
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 4000;
+
+server.use(middlewares);
+
+server.use(router);
+
+server.listen(port, () =>
+  console.log(`
+    Listening at:
+    http://localhost:${port}
+  `)
+);
